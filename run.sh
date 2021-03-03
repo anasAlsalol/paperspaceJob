b6bc417cf68cfe930277d8e9e10db0ea90b2f50a
Anas Salool <anasalsalool@gmail.com>
2021-03-03 14:37:31 +0200

    commint changes and run

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 apt-get update -y
 apt-get install wget curl -y
 # Download the Data
-curl -O goodls 'https://github.com/tanaikech/goodls/releases/download/v1.2.7/goodls_linux_amd64'
+wget -O goodls 'https://github.com/tanaikech/goodls/releases/download/v1.2.7/goodls_linux_amd64'
 chmod +x ./goodls
-bash ./goodls -u 'https://drive.google.com/drive/folders/1lE94p1pOqMiGcUgbU9-AjhKAXRbdQpd_?usp=sharing' --overwrite -key "AIzaSyCwL9Rnbt2PisdQYyASSGDTBMKUWcp9cxI"
+./goodls -u 'https://drive.google.com/drive/folders/1lE94p1pOqMiGcUgbU9-AjhKAXRbdQpd_?usp=sharing' --overwrite -key "AIzaSyCwL9Rnbt2PisdQYyASSGDTBMKUWcp9cxI"
 mv Quran_Data_ForBaseModel_5sec quran_data
 cd quran_data/quran
 unzip *.zip
